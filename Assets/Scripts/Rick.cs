6794d7b691b8baf4720f5af2fe45e0333f1a88b7
Farzad Afshari <far.afshari@gmail.com>
2015-01-24 12:56:23 -0500

    Physics stuff

--- Assets/Scripts/Rick.cs
+++ Assets/Scripts/Rick.cs
@@ -3,31 +3,25 @@ using System.Collections;
 
 public class Rick : MonoBehaviour {
 
-    public float rickSpeed;
+    public Vector2 rickSpeed;
 
 	// Use this for initialization
 	void Start () {
-	
+		rigidbody2D.AddForce (rickSpeed);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Vector2 v = rigidbody2D.velocity;
-        v.x = rickSpeed;
-	    rigidbody2D.velocity = v;
 
         //Debug.DrawRay((Vector2)(transform.position),-Vector2.up);
 	}
 
-    void OnCollisionEnter2D(Collision2D coll){
-        if(coll.gameObject.tag == "Sam"){
-            print("cockblocked");
-            rickSpeed = 0;
-        }
-        if(coll.gameObject.tag == "Bob"){
-            print("boing");
-            rickSpeed = -(2*rickSpeed); //only do this if Rick hits Bob from the side
-            //needs more code to check if hit from top
-        }
-    }
+    void OnCollisionEnter2D(Collision2D coll){}
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		if(coll.gameObject.tag == "Sam"){
+			print("cockblocked");
+			rigidbody2D.velocity = Vector2.zero;
+		}
+	}
 }
