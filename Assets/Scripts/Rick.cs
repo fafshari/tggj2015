0651a50e8a1ca3cf460ffcd4f2a54435f667c425
Farzad Afshari <far.afshari@gmail.com>
2015-01-24 16:19:51 -0500

    sounds and bg

--- Assets/Scripts/Rick.cs
+++ Assets/Scripts/Rick.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 
 public class Rick : MonoBehaviour {
-
+	
+	public Vector2 direction = Vector2.right;
     public Vector2 rickSpeed;
 
 	// Use this for initialization
@@ -22,6 +23,19 @@ public class Rick : MonoBehaviour {
 		if(coll.gameObject.tag == "Sam"){
 			print("cockblocked");
 			rigidbody2D.velocity = Vector2.zero;
+		}
+		if (coll.gameObject.tag == "Speedy") {
+			print ("lawdadasd");
+			float angle = coll.transform.eulerAngles.z * Mathf.Deg2Rad;
+			float sin = Mathf.Sin( angle );
+			float cos = Mathf.Cos( angle );
+
+			rigidbody2D.AddForce(new Vector2(direction.x * cos - direction.y * sin,
+			                                 direction.x * sin + direction.y * cos) * 1000);
+
+			AudioClip clip = Resources.Load ("slowmo") as AudioClip;
+			print(clip);
+			AudioSource.PlayClipAtPoint(Resources.Load ("slowmo") as AudioClip, transform.position, 1); 
 		}
 	}
 }
