1be9d6532a56be2bdaee03fc4d63de12ceebe0fe
Farzad Afshari <far.afshari@gmail.com>
2015-01-24 14:57:58 -0500

    Stuff

---
+++ Assets/RickRollCameraController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RickRollCameraController : MonoBehaviour {
+	public Rick _rick;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = _rick.transform.position;
+		transform.Translate (Vector3.forward * -10);
+	}
+}
